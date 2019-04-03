b95802141b60ed320ba98fce4ad05cda98916e0b
CaputoDavide93 <CaputoDavide93@gmail.com>
2019-04-03 18:01:22 +0100

    Add files via upload

---
+++ GamesWorshop Orders Script.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         GamesWorshop Orders Script
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Make the live of the account manager easy!
+// @author       Davide Caputo - CaputoDav@gmail.com
+//@match        https://www.games-workshop.com/*
+// @grant        none
+// ==/UserScript==
+//TEST
+
+(function() {
+    'use strict';
+var message =' Code: '+ document.querySelector("button.giftlist").getAttribute('data-skuid');
+    if (message.includes('991899')) {
+  message = document.querySelector("button.giftlist").getAttribute('data-skuid') + '<font color="red">06<font>';
+}else if (message.includes('992099')) {
+  message = document.querySelector("button.giftlist").getAttribute('data-skuid') + '<font color="red">06<font>';
+}
+if(document.querySelector('.product-info .btn-tooltip--isWebstoreExclusive') != null) message += '<b> ONLINE ONLY</b>'
+document.getElementsByTagName('h1')[1].innerHTML += '<br>________________<br>' + message
+
+
+})();
